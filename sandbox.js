da4004acb14182bff6d7bed0b65787d6aa1b20fd
Ayesh1998 <ayeshlak1998@gmail.com>
2020-06-14 21:06:14 +0530

    Compiled typescript to js

--- sandbox.js
+++ sandbox.js
@@ -2,5 +2,5 @@ var character = "mario";
 console.log(character);
 var inputs = document.querySelectorAll("input");
 inputs.forEach(function (input) {
-  console.log(input);
+    console.log(input);
 });
